62edb5dadc0f4d042f688434de59337d56dedc39
M.A.T <mat.chenari.computer@gmail.com>
2022-11-27 17:01:52 +0330

    base better structor for basecode

--- configs/urls.py
+++ configs/urls.py
@@ -1,4 +1,4 @@
-"""exercises URL Configuration
+"""hirkano URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls'))
+    path('',include('hirkano.blog.urls'))
 ]

305b16fe31604e8d0e008af89f45ee0e570c745a
M.A.T <mat.chenari.computer@gmail.com>
2022-12-03 14:18:50 +0330

    add slide show and service model to blog app

--- hirkano/blog/models.py
+++ hirkano/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class SlideShow(models.Model):
+    message = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    text = models.CharField(max_length=256)
+    image = models.ImageField()
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.CharField(max_length=256)

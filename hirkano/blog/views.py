ceb6a817d0dd3ccfe6b5092df5d7e03309e061fb
M.A.T <mat.chenari.computer@gmail.com>
2022-12-06 13:00:48 +0330

    commit form for message and add person tag to index

--- hirkano/blog/views.py
+++ hirkano/blog/views.py
@@ -17,7 +17,7 @@ from .form import(
 class IndexView(TemplateView):
     template_name = 'blog/index.html'
 
-    def get_context_data(self, request, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         context.update({
@@ -29,7 +29,7 @@ class IndexView(TemplateView):
             'blog':Blog.objects.all(),
             'contant_us':Contant_Us.objects.all(),
             'message':Message.objects.all(),
-            'messageForm':MessageForm(request.POST)
+            
         })
 
         return context

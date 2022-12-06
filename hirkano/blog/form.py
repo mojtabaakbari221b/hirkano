9cbf26f1cb9edffbd8733e3adaf161e4eae61b27
M.A.T <mat.chenari.computer@gmail.com>
2022-12-06 11:59:52 +0330

    deleting form

--- hirkano/blog/form.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.forms import ModelForm
-from models import Message
-
-class MessageForm(ModelForm):
-    class Meta:
-        model = Message
-        feilds = ['name', 'email', 'title', 'message']
-]

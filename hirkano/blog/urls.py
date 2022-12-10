0f9c4acd06d510482ab3955dad99e24f61f9fda1
M.A.T <mat.chenari.computer@gmail.com>
2022-12-10 14:34:32 +0330

    make happy-client model

--- hirkano/blog/urls.py
+++ hirkano/blog/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from .views import MessageCreateVeiw
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('', MessageCreateVeiw.as_view())
+    path('', views.IndexView.as_view(), name='index'),
 ]

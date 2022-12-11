f89cb7a933bbae7324e1070dcb0aa0eaeb75081c
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2022-12-11 22:02:52 +0330

    add CSRF_TRUSTED_ORIGINS setting

--- configs/settings.py
+++ configs/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 
 # Application definition
